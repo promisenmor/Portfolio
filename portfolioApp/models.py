c52baf98d34980dfc1070d363a7a33404de6e19b
Promise Nmor <nmorchibogu98@gmail.com>
2024-12-18 23:39:42 +0100

    made changes to the frontend

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField
+    tech_stack = models.CharField(max_length=200)
+    github_link = models.URLField(blank=True, null=True)
+    live_demo = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    
--- portfolioApp/views.py
+++ portfolioApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def portfolio(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolioApp/portfolio.html', {'projects': projects})
